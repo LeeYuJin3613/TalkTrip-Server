f29c4ebe3fabe59142145b0720be0090f0dd9728
yujin <dldbwls7115@naver.com>
2026-05-19 00:23:26 +0900

    일정 상세 불러오기 api 구현

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import receive_kakao_text
+from .views import receive_kakao_text, trip_plan_detail
 
 urlpatterns = [
     path('api/chat-files/upload/', receive_kakao_text),
+    path('api/trip-plans/<int:trip_plan_id>/',trip_plan_detail),
 ]
